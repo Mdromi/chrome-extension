4c4b3e419b0ce92061c3670c0882ee61fc69462c
mdromi <mdromi14@gmail.com>
2024-01-22 13:33:45 +0600

    [Pomodoro Timer] Timer Functionality Update

--- typeScript-react/pomodoro-timer/popup/js/handleTimer.js
+++ typeScript-react/pomodoro-timer/popup/js/handleTimer.js
@@ -6,7 +6,7 @@ const startBtn = document.getElementById("start");
 const resetBtn = document.getElementById("reset");
 
 // Function to update the time elements on the popup
-function updateTimeElement() {
+function updateTimeElement2() {
   chrome.storage.local.get(["timer"], (result) => {
     const time = result.timer ?? 0;
     console.log("time", time);
@@ -27,9 +27,34 @@ function updateTimeElement() {
   timeElement.textContent = `${currentTime}`;
 }
 
+function updateTimeElement() {
+  chrome.storage.local.get(["timer", "timeOption"], (res) => {
+    const totalMinutes = 75 - Math.ceil(res.timer / 60);
+    const hours = `${Math.floor(totalMinutes / 60)}`.padStart(2, "0");
+    const minutes = `${totalMinutes % 60}`.padStart(2, "0");
+    let seconds = "00";
+
+    if (res.timer % 60 !== 0) {
+      seconds = `${60 - (res.timer % 60)}`.padStart(2, "0");
+    }
+
+    secondsElement.textContent = seconds;
+    minutesElement.textContent = minutes;
+    hoursElement.textContent = hours;
+
+    // Update the current time element
+    const currentTime = new Date().toLocaleTimeString();
+    timeElement.textContent = `${currentTime}`;
+  });
+}
+
 // Initial update and set up interval for continuous updates
 updateTimeElement();
 setInterval(updateTimeElement, 1000);
+
+chrome.storage.local.get(["isRunning"], (res) => {
+  startBtn.textContent = res.isRunning ? "Puse Timer" : "Start Timer";
+});
 
 // Event listeners for the buttons
 startBtn.addEventListener("click", () => {
